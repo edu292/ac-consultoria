a46bdc1927b38a8bf03db7af3d43762f16153bbc
edu292 <eduskoroboatei@gmail.com>
2025-07-23 18:34:48 -0300

    Added brotli compression middleware

--- ac_consultoria/settings.py
+++ ac_consultoria/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django_brotli.middleware.BrotliMiddleware'
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
