bd58916b1dd76cb8dae18a7a784b42a2dec09cd9
edu292 <eduskoroboatei@gmail.com>
2025-07-23 19:04:37 -0300

    Testing fix for marker icons

--- ac_consultoria/settings.py
+++ ac_consultoria/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django_brotli.middleware.BrotliMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
