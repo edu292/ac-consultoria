3747dcbeca957fb22ffbc42bf691f1beda2420c8
edu292 <eduskoroboatei@gmail.com>
2025-07-26 20:52:27 -0300

    Updated the admin model for ocorrências

--- main/admin.py
+++ main/admin.py
@@ -6,3 +6,7 @@ from .resources import OcorrenciaResource
 @admin.register(Ocorrencia)
 class BookAdmin(ImportExportModelAdmin):
     resource_class = OcorrenciaResource
+    list_display = [field.name for field in Ocorrencia._meta.fields if not field.primary_key]
+    list_editable = [field.name for field in Ocorrencia._meta.fields if not field.primary_key]
+    list_display_links = None
+    search_fields = ['nome', 'placa', 'numero_documento']
